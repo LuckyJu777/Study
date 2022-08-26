3ebb19e646a6a3d7e70b8fd08a16b1efe5f1bb52
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-08-26 17:48:33 +0900

    조건문과 반복문

---
+++ src/chap3/ForMultiplicationTable.java
@@ -0,0 +1,10 @@
+package chap3;
+public class ForMultiplicationTable{
+	for (int m=2; m<=9; m++) {
+		System.out.println("***" + m + "단 ***");
+		for (int n=1; n<=9; n++) {
+		System.out.println(m + "x" + n +"=" + (m*n));
+	}
+   }
+ }
+}
---
+++ src/chap3/WhilePrintFrom1To10.java
@@ -0,0 +1,10 @@
+package chap3;
+public class WhilePrintFrom1To10{		//while증감식
+	public static void main(String[] args) {
+		int i = 1;
+		while(i<=10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
