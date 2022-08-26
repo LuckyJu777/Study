57ea157cf1f97691111d87267e224dc0943f8bc1
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-08-26 18:06:59 +0900

    조건문과 반복문

--- src/chap3/ForMultiplicationTable.java
+++ src/chap3/ForMultiplicationTable.java
@@ -1,5 +1,6 @@
 package chap3;
-public class ForMultiplicationTable{
+public class ForMultiplicationTable {
+	public static void main(String[] args) {
 	for (int m=2; m<=9; m++) {
 		System.out.println("***" + m + "단 ***");
 		for (int n=1; n<=9; n++) {
