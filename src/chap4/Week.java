ddf74a2abbfb0ff73becab80c041814825d8154d
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-08-28 01:11:02 +0900

    enum

---
+++ src/chap4/Week.java
@@ -0,0 +1,12 @@
+package chap4;
+public class Week{
+	public static void main(String[] args) { //public enum 열거타입이름{}
+		MONDAY,
+		TUESDAY,
+		WEDNESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY,
+		SUNDAY;
+	}
+}
