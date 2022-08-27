11b1f3bb6b82ff92d6f957aa4a5757e8a9621824
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-08-28 01:07:33 +0900

    ...

---
+++ src/sec07/exam01_enum/Week.java
@@ -0,0 +1,23 @@
+package sec07.exam01_enum;
+
+public enum Week {
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
+
+/**
+열거타입 변수;
+
+week today; 	
+week reservationDay;  >Week로 변수를 선언하면 다음과 같다. 	
+
+열거타입 변수 선언 >> 열거 상수 저장 >> 열거 상수는 단도으로 사용하 ㄹ수 업속 반드시 열거타입,열거 상수로 저장
+열거타입 변수 = 열거타입.열거상수;
+Week today = Week.SUNDAY;
+Week birthday = null;
+**/
