bc05b2e27c46f27342fa4f7fd720f105518ebd44
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-08-29 06:15:47 +0900

    (수정)
    (수정)

--- src/chap5/CarExample.java
+++ src/chap5/CarExample.java
@@ -1,18 +1,16 @@
-package chap5;
 public class CarExample{
 	public static void main(String[] args) {
-		//객체 생성
-		Car myCar = new Car(); // Car.java참조 객체 생성
+		GetterCar myCar = new GetterCar();
 		
-		//필드값 생성
-		System.out.println("제작회사: " +myCar.company);
-		System.out.println("모델명: " +myCar.model);
-		System.out.println("색깔: " +myCar.color);
-		System.out.println("최고속도: " +myCar.maxSpeed);
-		System.out.println("현재속도: " +myCar.speed);
+		myCar.setSpeed(-50);
 		
-		//필드값 변경
-		myCar.speed = 60;
-		System.out.println("수정된 속도: " +myCar.speed);
+		System.out.println("현재속도:" +myCar.getSpeed());
+		
+		myCar.setSpeed(60);
+		
+		if(!myCar.isStop()) {
+			myCar.setStop(true);
+		}
+		System.out.println("현재속도:" +myCar.getSpeed());
 	}
 }
