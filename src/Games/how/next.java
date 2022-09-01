2d65c84a5e233e3d1ae58641d29af1181fc1e39f
Juhyeong-Shin <unless01101@gmail.com>
2022-09-01 14:59:27 +0900

    Scanner.next()
    .

--- src/Games/how/next.java
+++ src/Games/how/next.java
@@ -14,24 +14,22 @@ import java.util.Scanner;
 class backback
 {
 	public static void main(String[] args)
-	{
+	{	String str1;
+		String str2;
+		
 		Scanner scan = new Scanner(System.in);
-		while(scan.hasNext()) {
-			String value = scan.next();
-			System.out.println(value);
-		}
+		while(scan.hasNextInt());
+			str1 = scan.next();
+			System.out.println(str1);
+			
+			str2 = scan.nextLine();
+			System.out.println(str2);
 	}
 }
-
-	class serve {
-		Scanner scan = new Scanner(System.in);
-		while(scan.hasNext()) {
-			String value = scan.nextLine();
-			System.out.print(value);
-		}
-	}
-
 /*
+ *hasNext()는 역할이 입력된 토큰이 있다면, true! 
+ *새로운 입력이 있을 때 까지 무한정 기다린다.
+ *hasNextInt() 같은경우는 값을 출력할 때 정수가 들어오면 true! 이다.
 *static void main을 선언하고 그 이후에는 클래스를 만들 때 main을 한번 했으니까
 *더 안써도 되는건지 아니면.. 이유가 무엇인지??
 */
