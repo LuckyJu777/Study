9bb20a5cc3e7d974afdda3e9c2b4867ed9a43b60
Juhyeong-Shin <unless01101@gmail.com>
2022-09-02 14:46:54 +0900

    햄버거를쌓기(시도중...)
    .

---
+++ src/Games/how/Menu2.java
@@ -0,0 +1,15 @@
+package how;
+
+public class Menu2{
+	void make() {	
+		String[] buger = {"빵","햄","치즈","패티","빵"};
+		for(int i=0; i < buger.length; i++) {
+			System.out.println("this.buger");
+		}
+	} 
+	}
+
+if(user2 == 1) {
+	System.out.println("햄버거를 만들어드릴게요!");
+	Menu2 myMenu2 = new Menu2;
+	myMenu2.make();
