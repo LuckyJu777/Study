177ef6bb7a4ae5d900ca2864b018ff8015ad4cc9
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-09-07 04:34:16 +0900

    수정중
    .

--- src/Use Example/Sum2.java
+++ src/Use Example/Sum2.java
@@ -2,24 +2,29 @@ import java.util.Scanner;
 
 public class Sum2{
 	public static void main(String[] args) {
-		System.out.println("입력하세요.");
-		Scanner scanner = new Scanner(System.in); //입력받기
-		int scanner2 = scanner.nextInt();			//입력받은 값을 scanner 변수에 넣기 
-		int[] number = new int[100];				//입력받은 값을 100번 담을 수 있는 배열통을 만들었다.
-		int size = number.length;					//size를 number배열로 인식 .
-		
-		boolean run = true;					
-		
-		for(int i = 0; i<size; i++){	//i = 0 i가 사이즈보다 높다면 i가 증감 루프 완성		
-			number[i] = scanner.nextInt();		//넘버백개까지 담을 수 있는데 입력한 값이 예를들어 3개면 i안에 3이라고 담긴다.
-			
-			if(run) {	//만약 
-				number[i] = scanner.nextInt(0);
-				System.out.println("멈춰");
-			}else {
-				System.out.println("멈춰2");
-			}
+		boolean run = true;
+		int sum = 0;
 
+//		int[] number2 = {number};
+		
+		while (run) {
+			System.out.println("입력하세요.");
+			Scanner scanner  = new Scanner(System.in);
+			int num = scanner.nextInt();
+			
+			if( num > 0 ) {
+				int [] number = new int[num];	// number 배열에 int배열에  num을 넣어준다.
+			} else if( num == 0) {	//0을 치면하면 프로그램 종료하면서, 내가 이때까지 입력한 값을 총합출력		
+				System.out.println("프로그램종료");
+					for(int element : number) {
+						sum += element;
+						System.out.println(sum);
+						}run = false;	
+			} else {
+				System.out.println("프로그램종료2");	
+				scanner.close();
+				break;
+			}
 		}
 	}
 }
