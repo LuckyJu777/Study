722ae96d25776d2668e289cd91a6038f56273ded
Juhyeong-Shin <unless01101@gmail.com>
2022-09-06 17:54:03 +0900

    홀수,짝수

--- src/Use Example/src/OddEven.java
+++ src/Use Example/src/OddEven.java
@@ -4,18 +4,16 @@ public class OddEven{
 	public static void main(String[] args) {
 		
 		System.out.println("홀수일까요 짝수일까요? ");
-			
 		Scanner scanner1 = new Scanner(System.in);
 		Scanner scanner2 = new Scanner(System.in);
-		int num = scanner1.nextInt();
-		String str = scanner2.nextLine();
 		
-		if(num == 1){					//조건문 홀수이면 => 홀수입니다.
+		int num = scanner1.nextInt();
+		if(num % 2 == 1){					//조건문 홀수이면 => 홀수입니다.
 			System.out.println("홀수입니다.");
-		}else if() {				//짝수이면 => 짝수입니다.
-			System.out.println(str+ "은 숫자가 아닙니다.숫자를 입력해주세요");
-		}else {								//숫자말고 문자를 입력하면 나오게 할 수는 없는건가
-			System.out.println("짝수입니다.");
-		}
+		}else {				//짝수이면 => 짝수입니다.
+			System.out.println("짝수입니다");
 	}
 }
+}
+
+//하고싶은것 : 문자입력하면 숫자가 아니라고 띄워주기 하고 싶은데 문자열과 숫자열을 한객체에 입력 받을 순 없는걸까??
