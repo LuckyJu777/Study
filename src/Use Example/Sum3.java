3a7a31ab83d301cf2b6f3863073edf03d31679bb
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-09-07 04:57:09 +0900

    이상한거만들어버림
    .

---
+++ src/Use Example/Sum3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Sum3{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("몇개의 숫자를 넣을것인지 입력해주세요.");
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		int sum = 0;
+		
+		System.out.println("숫자를 입력해주세요.");
+		for(int i = 0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+	
+		for(int element : arr) {
+			sum += element;
+		}
+		
+		sc.close();
+		
+		System.out.println(sum);
+	}
+}
