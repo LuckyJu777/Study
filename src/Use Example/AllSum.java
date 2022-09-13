649527ffdc9d2b8f321ea7a44490d87efa87e40e
Juhyeong-Shin <unless01101@gmail.com>
2022-09-13 12:43:35 +0900

    오늘수정

---
+++ src/Use Example/AllSum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class AllSum{
+	public static void main(String[] args) {
+		boolean run = true;
+		int sum = 0;
+		int[] number = null;
+		
+		while (run) {
+			System.out.println("1번을 누르면 배열의 숫자넣기 / 0을 누르면 합산합니다.");			
+			Scanner scanner  = new Scanner(System.in);								//사용자 입력값
+			int num = scanner.nextInt();											//nextint로 정수로 받아준다.
+			
+			switch(num) {							//swithch num에서 0을치면 합산을 한다.
+			case 0 : 
+				System.out.println("합산합니다.");
+				for(int element : number) {			//number 배열에 가져올 값이 존재하는지	-> element값에 저장한다. -> 실행문이 모두 실행되면 루프를 돌아  number에서 가져올 값이 있는지 본다.
+					sum += element;				
+					}
+					System.out.println(sum);
+					run = false;
+					break;
+			case 1 :								//1을 누르면
+				System.out.println("배열의 사이즈");	//배열사이즈를 정한다
+				int size = scanner.nextInt();		//size 배열의 사이즈 데이터 값을 사영자가 입력한다.
+				number = new int[size];				//size 데이터를 number 배열에 넣어준다.
+				System.out.println("들어갈 값을 넣어주세요.");
+				for(int i = 0; i<number.length; i++) {	//넘버 배열길이가 i보다 크다면 i증감 
+					number[i] = scanner.nextInt();		//i 매개변수를 적어준 두 값을 scanner로 받아옴 
+				}
+			}
+		}
+	}
+}
--- src/Use Example/Sum.java
+++ src/Use Example/Sum.java
@@ -1,50 +1,93 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Sum{
 	public static void main(String[] args) {
-//		
-//				System.out.println("숫자를 입력하세요 하나의 배열안에 인덱스와 데이터값을 넣어드립니다.");
-//				Scanner scanner  = new Scanner(System.in);	
-//				
-//				List<Integer> list = new ArrayList<Integer>();			//Integer classtype(Wrapper) int 원시형변수
-//				
-//				boolean run = true;
-//				while(run) {				//루프
-//					int num = scanner.nextInt();	//num을 데이터 값
-//					if(num == 0) {					//num 데이터 값을 0 넣어주면
-//						run = false;				// 멈춘다
-//					}
-//					list.add(num);					// num 데이터 추가, 객체 번지 참조하여 리스트가 순차적으로 인덱스 안에 저장	
-//				}
-//			
-//				for(int i = 0; i < list.size(); i++) {	//배열만들기, 저장된 데이터의 수만큼 인덱스를 지정, 루핑하여 배열안에   
-//					System.out.println(list.get(i));	//인덱스안의 데이터를 찾기 
-//				}
-//	}
-//	
-//
-//	static void serve() {
 		System.out.println("숫자를 입력하세요 하나의 배열안에 인덱스와 데이터값을 넣어드립니다.");
 		Scanner scanner2  = new Scanner(System.in);	//SCANNER
-		
-			boolean run2 = true;
-			int[] chart2;
-			while(run2) {
-				int num2 = scanner2.nextInt();
-				if(num2 == 0) {
-					run2 = false;					
-				}
-			System.out.println(num2);					//number선언에 실패
-			for(int i = 0; i < num2.length; i++) {
-				System.out.println();
+		List<Integer> list = new ArrayList<Integer>();
+			
+		boolean run = true;
+		while(run) {
+			int num = scanner2.nextInt();
+			if(num == 0) {
+				run = false;
 			}
+			list.add(num);
+		}						//0을 치면 멈춘다.
+								//홀수의 총합 짝수의 총합 위의 리스트에서 걸러서 적용 
+								//break문 없이
+		int num = list.size();	//저장된 총 객체수 얻기 , 홀수와 짝수의 총 갯수 리스트 출력 
+		System.out.println("총 객체수는? " + list.size());
+		System.out.println(list.add(num));
+		
+		
+		for(int i = 0; i<list.size(); i++) {
+			int str = list.get(num);		// i 정의 해줬는데 왜 말안들어?
+			System.out.println("총 합계는?" + str);
+		}
+
+	
+		
+		//입력한 값들 중 홀수 리스트 출력
+		
+		
+		
+	
+	}	
+}		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*switch(1) {
+		case 0 :
+			int num = list.size();	//저장된 총 객체수 얻기 , 홀수와 짝수의 총 갯수 리스트 출력 
+			System.out.println("총 객체수는? " + list.size());
+			System.out.println();
+			
+			int sum = list.get(num);
+			for(int i = 0; i<list.size(); i++) {
+				String str = list.get(i);		// i 정의 해줬는데 왜 말안들어?
+			}
+			System.out.println("총 합계는?" + sum);
+		
+		case 2 :	//입력한 값들 중 홀수 리스트 출력
+			
+		case 3 :	//입력한 값들 중 짝수 리스트 출력
+		
+		case 4 :  	//홀수의 총 갯수
+			
+		case 5 : 	//짝수의 총 갯수
+			
+		case 6 :	//홀수들의 총 합
+		
+		case 7 : 	//짝수들의 총 합
+			
+		case 8 : 	//홀수의 총합과 짝수의 총합을 비교하여 큰 수 출력
+			
+		}
+		
+			
 		}
 	}				
-}
 				
-				
+				*/
 				
 				
 				
