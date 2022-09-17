d218478caa096289a793a537a319dcb991fed18e
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-09-17 23:36:00 +0900

    untiljava머시기라고뜨는데
    수정해야함

---
+++ src/chap11/Member.java
@@ -0,0 +1,18 @@
+public class Member {
+	public String id;
+	public Member(String id) {
+		this.id = id;
+	}
+	
+	@Override
+	
+public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member membeer = (Member) obj;
+			if(id,equals(member.)) {
+				return.true;
+				}
+			}
+		return false;
+	}
+}
