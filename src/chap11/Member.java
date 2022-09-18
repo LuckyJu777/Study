b12ff02aa186be0524acabe8f2bb87b2840e19af
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-09-18 22:16:40 +0900

    깊은복제하는중..470p

--- src/chap11/Member.java
+++ src/chap11/Member.java
@@ -1,18 +1,23 @@
 public class Member {
 	public String id;
+	
 	public Member(String id) {
 		this.id = id;
 	}
 	
 	@Override
-	
-public boolean equals(Object obj) {
+	public boolean equals(Object obj) {
 		if(obj instanceof Member) {
-			Member membeer = (Member) obj;
-			if(id,equals(member.)) {
-				return.true;
-				}
+			Member member = (Member) obj;
+			if(id.equals(member.id)) {
+				return true;
 			}
+		}
 		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return id.hashCode();
 	}
 }
---
+++ src/chap11/deep clone/Car.java
@@ -0,0 +1,7 @@
+public class Car{
+	public String model;
+	
+	public Car(String model) {
+		this.model = model;
+	}
+}
