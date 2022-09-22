f67d00d0e8667a2b31229e0cc0b8517d883c81dc
Juhyeong-Shin <unless01101@gmail.com>
2022-09-22 13:47:22 +0900

    배열안의데이터
    데이터를 합산하는게 문제네.. 데이터를 꺼내려하지말고 for문으로 하나 더 만들어보기

--- src/Use Example/OddOrEven/Sum.java
+++ src/Use Example/OddOrEven/Sum.java
@@ -8,116 +8,63 @@ public class Sum{
 		System.out.println("숫자를 입력하세요 하나의 배열안에 인덱스와 데이터값을 넣어드립니다.");
 		Scanner scanner2  = new Scanner(System.in);	//SCANNER
 		List<Integer> list = new ArrayList<Integer>();
-			
+		List<Integer>odd_list = new ArrayList<Integer>();
+		List<Integer>even_list = new ArrayList<Integer>();
+		int odd_tot = 0;
+		int even_tot = 0;
+		
 		boolean run = true;
+
 		while(run) {
 			int num = scanner2.nextInt();
 			if(num == 0) {
-				run = false;
-			}
+				break;
+			}						//0을 넣으면 break;
 			list.add(num);			//배열안에 데이터 넣어주기
-		}						
-
-		
-		int num = list.size();		//총 객체수
-		System.out.println("총 객체수는? " + list.size());
-		
-		int sum2 = 0;
-		int[] number2 = null;
-		for(int element : number2) {			//number 배열에 가져올 값이 존재하는지	-> element값에 저장한다. -> 실행문이 모두 실행되면 루프를 돌아  number에서 가져올 값이 있는지 본다.
-			sum2 += element;				
-			}
-			System.out.println(sum2);
-
-		for(int i = 0; i<list.size(); i++) {
-			int str = list.get(i);		
-			System.out.println("데이터 반환 - " + str);			//객체타입반환
-		}
-		
-			if(num % 2 == 1){					//조건문 홀수이면 => 홀수입니다.
-				System.out.println("홀수입니다.");
-				int i = 0;
-				int str1 = list.get(i);
-				System.out.println(":" + str1);
-				
-			}else {				//짝수이면 => 짝수입니다.
-				System.out.println("짝수입니다");
-				
-		}
-
-	
-		
-		//입력한 값들 중 홀수 리스트 출력
-		
-		
-		
-	
-	}	
-}		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*switch(1) {
-		case 0 :
-			int num = list.size();	//저장된 총 객체수 얻기 , 홀수와 짝수의 총 갯수 리스트 출력 
-			System.out.println("총 객체수는? " + list.size());
-			System.out.println();
+		}	
+			System.out.println("총 객체수는? " + list.size());	//1	총객체 수
 			
-			int sum = list.get(num);
+			for(int i = 0; i<list.size(); i++) { 			//2 총 데이터 반환
+				int str1 = list.get(i); 
+				System.out.println("데이터 반환 - " + str1);	
+			  }	
+			System.out.println(); 
+			
+			System.out.println("입력한 홀수의 값을 출력합니다.");	//3 홀수 데이터 반환
 			for(int i = 0; i<list.size(); i++) {
-				String str = list.get(i);		// i 정의 해줬는데 왜 말안들어?
+				int str2 = list.get(i);
+				if(str2%2!=0){
+				System.out.println("데이터 반환 - " + str2);
+				int str3 = odd_list.get(i);						//홀수를 배열안에 넣어준다.
+				odd_tot += str3;									//배열안의 데이터를 += 더하기더하기 해서 다른변수에 넣어주기
+				odd_list.add(odd_tot);								//아니 여기서 막히면 어덕해유
+				}
 			}
-			System.out.println("총 합계는?" + sum);
+			System.out.println("홀수의 총합을 출력합니다." + odd_tot);
+			System.out.println(); 
+			
+			System.out.println("입력한 짝수의 값을 출력합니다.");	//4 짝수 데이터 반환
+			for(int i = 0; i<list.size(); i++) {
+				int str3 = list.get(i);
+				if(str3%2 == 0){
+				System.out.println("데이터 반환 - " + str3);	
+				} 
+			}	
+			/*
+			 * System.out.println(); //Integer를 빼
+			 * System.out.println("입력한 값들 중 홀수값의 총 합계 리스트 출력"); for(int i = 0;
+			 * i<list.size(); i++) { int str4 = odd_list.get(i); //일단 배열 넣고 if(str4%2!=0){
+			 * //배열안에 홀수면 int num1 = odd_list.get(i); //5 홀수배열안에서 총 합계
+			 * 
+			 * } } System.out.println("홀수의 값 합계 입니다." + odd_list.size() );
+			 * System.out.println();
+			 */
+		 
 		
-		case 2 :	//입력한 값들 중 홀수 리스트 출력
-			
-		case 3 :	//입력한 값들 중 짝수 리스트 출력
 		
-		case 4 :  	//홀수의 총 갯수
-			
-		case 5 : 	//짝수의 총 갯수
-			
-		case 6 :	//홀수들의 총 합
 		
-		case 7 : 	//짝수들의 총 합
-			
-		case 8 : 	//홀수의 총합과 짝수의 총합을 비교하여 큰 수 출력
-			
-		}
 		
-			
-		}
-	}				
-				
-				*/
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-					
+	
+		  }	
+	}
+
