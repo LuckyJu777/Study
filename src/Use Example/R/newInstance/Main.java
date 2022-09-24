bfd03c850768e678bc4c342ca4240740a273cfe3
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-09-24 14:43:38 +0900

    Main수정
    수정해야합니다

--- src/Use Example/R/newInstance/Main.java
+++ src/Use Example/R/newInstance/Main.java
@@ -3,8 +3,7 @@ package R.newInstance;
 import java.lang.reflect.InvocationTargetException;
 
 public class Main{
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-		
+	public static void main(String[] args) throws 
 		try {
 			Class clazz1 = Class.forName("NewInstance.BodyInfo");		//forName 정적메소드, 전체 이름(패키지가 포함된 이름)을 매개값으로 받ㄷ고 class객체를 리턴한다.
 			Info info = (Info) clazz1.getDeclaredConstructor().newInstance();
@@ -14,12 +13,12 @@ public class Main{
 			e.printStackTrace();
 		}
 		
-		try {
-			Class clazz2 = Class.forName("NewInstance.PersonalInfo");
-			Info info = (Info) clazz2.getDeclaredConstructor().newInstance();
-			info.myInfo();
-		}catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			Class clazz2 = Class.forName("NewInstance.PersonalInfo");
+//			Info info = (Info) clazz2.getDeclaredConstructor().newInstance();
+//			info.myInfo();
+//		}catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
 	}
 }		//여기는 왜 throw가 없어??? 440p 
