7867bd2f6e1c6aa2f9bbd0b54b999f70e9bb1c74
Big-Angry-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2022-09-27 01:14:28 +0900

    <try>
    .

---
+++ src/Use Example/Member/MemberService.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MemberService {					//class {}
+	public void main(String[] args) {			//main method() {} 
+		System.out.println("아이디와 비밀번호를 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+		List<String> Id = new ArrayList<String>();	//id 리스트 넣어줌
+		List<String> Password = new ArrayList<String>();	// Password 리스트 넣어줌
+	}
+	
+	String login(String id, String password) {	//method()
+		if(true) {
+			String Id = "hong";
+			
+		} else {
+			
+		}
+			
+	}
+	
+	String logout(String id) {
+		
+	}
+}
