affbc291fa09d2c3ebe51201d2316d76179841e3
Juju <unless01101@gmail.com>
2022-11-01 07:17:02 +0900

    <Editing>
    .

---
+++ src/Use Example/Six/MemberDao.java
@@ -0,0 +1,72 @@
+package Six;
+
+public class MemberDao {
+	
+	private String name;
+	private String id;
+	private String password;
+	private int age;
+	
+	MemberDao(){
+		
+	}
+	
+	MemberDao(String name){
+		this.name = name;
+	}
+
+	MemberDao(String name, String id) {
+		this.name = name;
+		this.id = id;
+		// TODO Auto-generated constructor stub
+	}
+	
+//	MemberDao(String name, String id, String password){
+//		this.name = name;
+//		this.id = id;
+//		this.password = password;
+//	}	
+	
+	MemberDao(String name, String id, String password){
+		this.setName(name);
+		this.setId(id);
+		this.setPassword(password);
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/Use Example/Six/Memberlogin.java
@@ -0,0 +1,15 @@
+package Six;
+
+public class Memberlogin {
+	boolean login(String id, String password) {
+		if(id.equals("hong") && password.equals("12345")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+		
+	void logout(String id) {
+		System.out.println("로그아웃");
+	}
+}
