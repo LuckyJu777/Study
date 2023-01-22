2757de238b4688b3c9f2b733671582bb4f381e5b
Big-Stupid-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2023-01-22 23:54:33 +0900

    [Bronze V] Title: X보다 작은 수, Time: 568 ms, Memory: 30692 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -4,18 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
  
-		int N = in.nextInt();
-		int X = in.nextInt();
-		int arr[] = new int[N];
+		int A = in.nextInt();
+		int C = in.nextInt();
+		int arr[] = new int[A];
         
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < A; i++) {
 			arr[i] = in.nextInt();
 		}
  
 		in.close();
         
-		for (int i = 0; i < N; i++) {
-			if (arr[i] < X) {
+		for (int i = 0; i < A; i++) {
+			if (arr[i] < C) {
 				System.out.print(arr[i] + " ");
 			}
 		}
