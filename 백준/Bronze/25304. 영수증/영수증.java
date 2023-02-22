406257a8c4372b2a616084df015f68c460c7787c
Big-Juju <107597057+Juhyeong-Shin@users.noreply.github.com>
2023-02-22 23:20:58 +0900

    [Bronze V] Title: 영수증, Time: 224 ms, Memory: 17904 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt();   //총금액
-        int N = sc.nextInt(); //종류 수
 
-        int C = 0;
+        Scanner sc = new Scanner(System.in);
 
-        for(int i=0; i<N; i++){
-            int a =  sc.nextInt(); //물건가격
-            int b =  sc.nextInt(); //개수
-            C +=a*b;
+        int total =  sc.nextInt();
+        int count = sc.nextInt();
+
+        for(int i = 0; i< count; i++){
+            int price = sc.nextInt();
+            int num = sc.nextInt();
+
+            total = total - (price * num);
         }
 
-        if(X == C){
-            System.out.println("Yes");
-        }else{
-            System.out.println("No");
-        }
+        if(total == 0) System.out.println("Yes");
+        else System.out.println("No");
+
+
     }
 }
