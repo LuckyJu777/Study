89ff514d7a4a6d424743643be9f0e3f29543e6a3
Big-Juju <unless01101@gmail.com>
2023-04-06 23:59:25 +0900

    삭제

--- src/Use Example/Test/ForEx0.java
+++ src/Use Example/Test/ForEx0.java
@@ -1,13 +1,13 @@
-package Test;
+	package Test;
 
-public class ForEx0 {
-	public static void main(String[] args) {
-		
-		System.out.println("조회함수를 출력합니다.");
-		
-		ForEx forforE = new ForEx();
-		forforE.ForEx1();
-
-		//null값을 받지 않으려면 받는
+	public class ForEx0 {
+		public static void ForEx0(String[] args) {
+			int numArray[]= {1, 2, 3, 4, 5, 6,7,8,9,10};
+			for(int i=0; i<numArray.length; i++) {
+				if(i % 5 == 1) {
+					System.out.print(numArray[i]);
+				}
+				System.out.println();
+			}
+		}
 	}
-}
