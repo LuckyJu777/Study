e88091f09ac12dd5693b3310c18572ba31546281
Big-juju <qhrrl3891@gmail.com>
2023-04-09 21:14:46 +0900

    baekjoon

--- src/Use Example/BaekJoon/Main.java
+++ src/Use Example/BaekJoon/Main.java
@@ -7,20 +7,31 @@ import java.util.Scanner;
 
     public class Main {
         public static void main(String[] arg) {
-        Scanner sc = new Scanner(System.in);
-        int H = sc.nextInt();
-        int M = sc.nextInt();
-        int CookTime = sc.nextInt();    //요리하는데 주어진 시간
-        sc.close();
-        //        1. A(시)와 B(분)을 하나의 분으로 변환한 뒤, C를 더하고 나온 결과 값을 다시 시와 분 으로 변환하는 방법
-        int min = H * 60 + M;
-        min = min + CookTime;
-
-        int hour = (min / 60) % 24;    //시
-        int minute = min % 60;      //분
-
-        System.out.println(hour +" "+ minute);
-    }
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                int A = sc.nextInt();
+                int B = sc.nextInt();
+                if (A == 0 && B == 0) {
+                    sc.close();
+                    break;
+                }
+                System.out.println(A + B);
+            }
+        }
+//        Scanner sc = new Scanner(System.in);
+//        int H = sc.nextInt();
+//        int M = sc.nextInt();
+//        int CookTime = sc.nextInt();    //요리하는데 주어진 시간
+//        sc.close();
+//        //        1. A(시)와 B(분)을 하나의 분으로 변환한 뒤, C를 더하고 나온 결과 값을 다시 시와 분 으로 변환하는 방법
+//        int min = H * 60 + M;
+//        min = min + CookTime;
+//
+//        int hour = (min / 60) % 24;    //시
+//        int minute = min % 60;      //분
+//
+//        System.out.println(hour +" "+ minute);
+//    }
 }
 //        public static void main(String[] args) {
 //            Scanner sc = new Scanner(System.in);
