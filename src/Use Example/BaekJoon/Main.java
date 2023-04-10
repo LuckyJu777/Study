0921442cc9937267fc2e43da55180aec48791ed0
Big-juju <qhrrl3891@gmail.com>
2023-04-10 23:46:50 +0900

    baekjoon

--- src/Use Example/BaekJoon/Main.java
+++ src/Use Example/BaekJoon/Main.java
@@ -8,15 +8,41 @@ import java.util.Scanner;
     public class Main {
         public static void main(String[] arg) {
             Scanner sc = new Scanner(System.in);
-            while (true) {
-                int A = sc.nextInt();
-                int B = sc.nextInt();
-                if (A == 0 && B == 0) {
-                    sc.close();
-                    break;
+
+            int[] arr = new int[sc.nextInt()];
+            for (int i = 0; i < arr.length; i++) { // 기본배열 생성
+                arr[i] = i + 1;
+            }
+
+            int N = sc.nextInt();
+            for (int i = 0; i < N; i++) {
+                int order1 = sc.nextInt() - 1;
+                int order2 = sc.nextInt() - 1;
+
+                while (order1 < order2) {
+                    int temp = arr[order1];
+                    arr[order1++] = arr[order2];
+                    arr[order2--] = temp;
                 }
-                System.out.println(A + B);
+            }
+
+            String ret = "";
+            for (int j = 0; j < arr.length; j++) {
+                ret += arr[j] + " ";
+            }
+            System.out.print(ret.trim());
+            sc.close();
         }
+    }
+//            Scanner sc = new Scanner(System.in);
+//            while (true) {
+//                int A = sc.nextInt();
+//                int B = sc.nextInt();
+//                if (A == 0 && B == 0) {
+//                    sc.close();
+//                    break;
+//                }
+//                System.out.println(A + B);
 //        Scanner sc = new Scanner(System.in);
 //        int H = sc.nextInt();
 //        int M = sc.nextInt();
